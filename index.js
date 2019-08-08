57e7f9d60b58315eb950b2bee73c8bcb27d55be6
Temitope Akintoye <ruth_olukunle@yahoo.com>
2019-08-08 00:50:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const currentUser = 'Jayne El-di-moore';// Write your code in this file!
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
